0e7833d24953aaa22aefe7f4f70d124781b09169
edilsonchaves <edilson.montenegro@gmail.com>
2026-02-27 21:22:28 -0300

    Stop timer after finish the game with victory

--- Assets/Scripts/Timer/TimerController.cs
+++ Assets/Scripts/Timer/TimerController.cs
@@ -1,29 +1,33 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using Mirror;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
+
 public class TimerController : MonoBehaviour
 {
     private Action _onFinishedTimeCallback;
     [SerializeField] private float _totalTimeSeconds;
     [SerializeField] private TextMeshProUGUI _timerView;
-
+    private CancellationTokenSource  _cts;
      public void StartTime(Action FinishAction)
     {
+        _cts = new CancellationTokenSource ();
         _onFinishedTimeCallback = FinishAction;
-        TimerWork();
+        TimerWork(_cts.Token).Forget();
     }
 
-    private async UniTaskVoid TimerWork()
+    private async UniTaskVoid TimerWork(CancellationToken  token)
     {
         float elapsedTime = 0;
         while (elapsedTime < _totalTimeSeconds)
         {
+            token.ThrowIfCancellationRequested();
             elapsedTime += Time.deltaTime; 
             var timespan = TimeSpan.FromSeconds(_totalTimeSeconds - elapsedTime);
             _timerView.SetText($"{timespan.Minutes:D2}:{timespan.Seconds:D2}");
@@ -39,5 +43,11 @@ public class TimerController : MonoBehaviour
         _timerView.SetText($"00:00");
         FindAnyObjectByType<GameplayManagerNetworking>().Cmd_Defeat();
         _onFinishedTimeCallback?.Invoke();
+    }
+
+    public void StopTimer()
+    {
+        _cts.Cancel();
+        _cts.Dispose();
     }
 }
